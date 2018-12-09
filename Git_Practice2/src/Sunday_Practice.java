a91f7b5fb7c621976e01c7493ada02d33cd33509
cexy <cexy@saras-mbp.fios-router.home>
2018-12-09 14:01:49 -0500

    SUN - 103 2nd changes

--- Git_Practice2/src/Sunday_Practice.java
+++ Git_Practice2/src/Sunday_Practice.java
@@ -3,5 +3,6 @@ public class Sunday_Practice {
 	public static void main(String[] args) {
 		System.out.println("great day today!");
 		System.out.println("making changes");
+		System.out.println("2nd changes");
 	}
 }
